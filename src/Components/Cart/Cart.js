4f343026230f07f1dc18abe5d190e0049f0b65cf
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-12 09:49:03 +0600

    all more

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,22 +5,26 @@ const Cart = (props) => {
 
  //access from props
  const{cart}=props;
-
+ let totalQuantity=0;
  //mapping the cart array
- const total=cart.reduce((previous,product)=>previous+product.price,0);
+//  const total=cart.reduce((previous,product)=>previous+product.price,0);
 
- // let total=0;
- // for(const product of cart){
- //  total=total+product.price;
- // }
-
- const shipping=0;
+ let total=0;
+ for(const product of cart){
+ if(!product.quantity){
+   product.quantity=1;
+ }
+  total=total+product.price*product.quantity;
+  totalQuantity=totalQuantity+product.quantity;
+ }
+ 
+ const shipping=total>0?15:0;
  const tax=(total+shipping)*0.1;
  const grandTotal=total+shipping+tax;
  return (
   <div>
      <h3>Order Summary: </h3>
-     <h5>{props.cart.length}</h5>
+     <h5>{totalQuantity}</h5>
      <br />
      <p>Total:{total.toFixed(2)}</p>
      <p>Shipping:{shipping}</p>
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -18,19 +18,25 @@ const Shop = () => {
    .then(res=>res.json())
    .then(data=>{
      setProducts(data)
-     console.log('products received')
+    //  console.log('products received')
    })
  },[])
   
  useEffect(()=>{
-   console.log('local storage called')
+  //  console.log('local storage called')
    if(products.length){
      const savedCart=getStoredCart();
      const storedCart=[];
 
    for (const key in savedCart){
      const addedProduct=products.find(product=>product.key===key);
-     storedCart.push(addedProduct)
+     if(addedProduct){
+      const quantity=savedCart[key];  
+      addedProduct.quantity=quantity;
+      console.log(addedProduct)
+      storedCart.push(addedProduct)
+     }
+     
    }
    setCart(storedCart);
    }
