55d2a1aa08de6ac5ea282991ff7be58b9dbc5fdc
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-22 07:18:28 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Header from './Components/Header/Header';
-import Shop from './Components/Shop/Shop';
 
+import Shop from './Components/Shop/Shop';
+import OrderReview from './Components/OrderReview/OrderReview';
+import Inventory from './Components/Inventory/Inventory'
+import NotFound from './Components/NotFound/NotFound'
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+              <Shop></Shop>
+          </Route>
+          <Route  path="/shop">
+              <Shop></Shop>
+          </Route>
+          <Route  path="/review">
+              <OrderReview></OrderReview>
+          </Route>
+           <Route  path="/inventory">
+              <Inventory></Inventory>
+          </Route>
+           <Route  path="*">
+              <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
