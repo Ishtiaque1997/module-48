cd7bcf6986df7a4c587acb687fde2bf3ae5c8da3
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-22 10:13:02 +0600

    some more to go

--- src/Components/OrderReview/OrderReview.js
+++ src/Components/OrderReview/OrderReview.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
  const [products]=useProducts();
  const[cart,setCart]=useCart(products);
+
  const handleRemove=(key)=>{
    const newCart=cart.filter(product=>product.key!==key);
-   setCart(newCart)
+   setCart(newCart);
+   removeFromDb(key);
  }
+
  return (
   <div className='shop-container'>
    <div className='product-container'>
